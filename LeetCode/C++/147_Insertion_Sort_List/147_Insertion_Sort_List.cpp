92a8298179086709d6ef80794b9d34cdc5b80f17
cx <nihao_cx@163.com>
2020-04-03 08:32:40 +0800

    147_Insertion_Sort_List

---
+++ LeetCode/C++/147_Insertion_Sort_List/147_Insertion_Sort_List.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <list>
+#include <sstream>
+#include <memory>
+#include <algorithm>
+
+using namespace std;
+
+/*
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    /*
+     * 插入排序思想为维护一个有序区间，然后将无序区间的数插入有序区间相应位置。
+     */
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode sorted(0), *prev(&sorted);
+        while(head){
+            ListNode* next = head->next;
+            if(prev->val > head->val)
+                prev = &sorted;
+            while(prev->next && prev->next->val <= head->val)
+                prev = prev->next;
+            head->next = prev->next;
+            prev->next = head;
+            head = next;
+        }
+        return sorted.next;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+ListNode* stringToListNode(string input) {
+    // Generate list from the input
+    vector<int> list = stringToIntegerVector(input);
+
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+string listNodeToString(ListNode* node) {
+    if (node == nullptr) {
+        return "[]";
+    }
+
+    string result;
+    while (node) {
+        result += to_string(node->val) + ", ";
+        node = node->next;
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        ListNode* head = stringToListNode(line);
+        
+        ListNode* ret = Solution().insertionSortList(head);
+
+        string out = listNodeToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
