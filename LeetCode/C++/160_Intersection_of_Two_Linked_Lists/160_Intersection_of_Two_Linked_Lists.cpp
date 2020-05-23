b2183e64c885e36349d5ec530b7def1244d1791c
cx <nihao_cx@163.com>
2020-05-23 22:52:07 +0800

    160_Intersaction_of_Two_Linked_Lists

---
+++ LeetCode/C++/160_Intersection_of_Two_Linked_Lists/160_Intersection_of_Two_Linked_Lists.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(!headA || !headB)
+            return nullptr;
+        ListNode *pA(headA), *pB(headB);
+        bool redirectA(false), redirectB(false);
+        while(pA && pB && pA != pB){
+            if(pA->next){
+                pA = pA->next;
+            }else if(!redirectA){
+                pA = headB;
+                redirectA = true;
+            }else{
+                pA = nullptr;
+            }
+            if(pB->next){
+                pB = pB->next;
+            }else if(!redirectB){
+                pB = headA;
+                redirectB = true;
+            }else{
+                pB = nullptr;
+            }
+        }
+        return pA;
+    }
+};
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+ListNode* stringToListNode(string input) {
+    // Generate list from the input
+    vector<int> list = stringToIntegerVector(input);
+
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+string listNodeToString(ListNode* node) {
+    if (node == nullptr) {
+        return "[]";
+    }
+
+    string result;
+    while (node) {
+        result += to_string(node->val) + ", ";
+        node = node->next;
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        int intersectVal = stringToInteger(line);
+        getline(cin, line);
+        ListNode* listA = stringToListNode(line);
+        getline(cin, line);
+        ListNode* listB = stringToListNode(line);
+        getline(cin, line);
+        int skipA = stringToInteger(line);
+        getline(cin, line);
+        int skipB = stringToInteger(line);
+        
+        ListNode* ret = Solution().getIntersectionNode(listA, listB);
+
+        string out = listNodeToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
