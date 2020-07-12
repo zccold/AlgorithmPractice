f6f708d78b41789a5ebde2eae978692364c3334a
乡魂 <xianghun.cx@alibaba-inc.com>
2020-07-12 11:58:41 +0800

    41_First_Missing_Positive

---
+++ LeetCode/C++/41_First_Missing_Positive/41_First_Missing_Positive.cpp
@@ -0,0 +1,62 @@
+#include <assert.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < n; i++)
+            // 如果这里为if，则当testcast = [-1,4,3,1], 返回值为1，而期望值为2.
+            while(nums[i] > 0 && nums[i] <= n && nums[i]-1 != i && nums[i] != nums[nums[i]-1])
+                swap(nums[i], nums[nums[i]-1]);
+        for(int i = 0; i < n; i++)
+            if(nums[i]-1 != i)
+                return i+1;
+        return 0;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+        
+        int ret = Solution().firstMissingPositive(nums);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
