dd66c595ad3776bd0a5ed6604d9227e7a073e926
scepter <zc_cold@hotmail.com>
2020-07-28 23:24:26 +0800

    p0104_Maximum_Depth_of_Binary__Tree

---
+++ LeetCode/Go/p0104_Maximum_Depth_of_Binary_ Tree/code.go
@@ -0,0 +1,21 @@
+package p0104_Maximum_Depth_of_Binary__Tree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
