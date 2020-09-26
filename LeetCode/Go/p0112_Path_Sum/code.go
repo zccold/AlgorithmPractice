4a59b84adc7a7fc8c8aff781bf564f8da8559218
scepter <zc_cold@hotmail.com>
2020-09-26 15:31:47 +0800

    去掉一个无用的判断

--- LeetCode/Go/p0112_Path_Sum/code.go
+++ LeetCode/Go/p0112_Path_Sum/code.go
@@ -14,8 +14,5 @@ func hasPathSum(root *TreeNode, sum int) bool {
 		return sum == root.Val
 	}
 	sum2 := sum - root.Val
-	if sum2 == 0 && root.Left == nil && root.Right == nil {
-		return true
-	}
 	return hasPathSum(root.Left, sum2) || hasPathSum(root.Right, sum2)
 }
