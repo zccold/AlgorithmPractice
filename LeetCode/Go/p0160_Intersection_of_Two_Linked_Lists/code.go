581ee93e26c6e537602bd484a39c1c57d5ae3f4a
scepter <zc_cold@hotmail.com>
2020-10-19 22:51:28 +0800

    p0160_Intersection_of_Two_Linked_Lists

---
+++ LeetCode/Go/p0160_Intersection_of_Two_Linked_Lists/code.go
@@ -0,0 +1,28 @@
+package p0160_Intersection_of_Two_Linked_Lists
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	if headA == nil || headB == nil {
+		return nil
+	}
+	pa := headA
+	pb := headB
+	//如果没有交汇，会在遍历到对方链的尾部时达成nil==nil
+	for pa != pb {
+		if pa != nil {
+			pa = pa.Next
+		} else {
+			pa = headB
+		}
+		if pb != nil {
+			pb = pb.Next
+		} else {
+			pb = headA
+		}
+	}
+	return pa
+}
