fc9f0732f6de6a310756a08c658b904677b8d6ce
scepter <zc_cold@hotmail.com>
2020-12-24 23:46:22 +0800

    update 0003

--- LeetCode/Go/p0003_Longest_Substring_Without_Repeating_Characters/code.go
+++ LeetCode/Go/p0003_Longest_Substring_Without_Repeating_Characters/code.go
@@ -1,21 +1,20 @@
 package p0003_Longest_Substring_Without_Repeating_Characters
 
 func lengthOfLongestSubstring(s string) int {
-	lastAppeared := make(map[byte]int, 64) //字符上一次出现的index
-	leftIndex := -1                        //当前计算长度时使用的最左下标
-	maxLength := 0
+	lastIndexMap := make(map[byte]int) //字符上一次出现的index
+	left := -1                         //当前计算长度时使用的最左下标-1
+	res := 0
 	for i := 0; i < len(s); i++ {
-		if lastIndex, ok := lastAppeared[s[i]]; ok && lastIndex >= leftIndex {
-			//当前字符出现过，且位置在leftIndex的右侧，更新leftIndex
-			leftIndex = lastIndex
+		c := s[i]
+		if lastIndex, ok := lastIndexMap[c]; ok && lastIndex > left {
+			left = lastIndex //当前字符出现过，且位置在leftIndex的右侧，更新leftIndex
+		} else {
+			tmp := i - left
+			if tmp > res {
+				res = tmp
+			}
 		}
-		//检查当前长度是否大于maxLength
-		length := i - leftIndex
-		if length > maxLength {
-			maxLength = length
-		}
-		//更新当前字符的lastAppeared
-		lastAppeared[s[i]] = i
+		lastIndexMap[c] = i
 	}
-	return maxLength
+	return res
 }
