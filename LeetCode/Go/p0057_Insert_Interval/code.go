70a2c98d3354c0c854f4e4bae2791490bebed490
scepter <zc_cold@hotmail.com>
2023-01-17 10:52:48 +0800

    p57

--- LeetCode/Go/p0057_Insert_Interval/code.go
+++ LeetCode/Go/p0057_Insert_Interval/code.go
@@ -7,22 +7,31 @@ func insert(intervals [][]int, newInterval []int) [][]int {
 
 	res := make([][]int, 0)
 	tmp := newInterval
+	flag := false // 是否通过情况1直接把后一半的值都append进去了
 	for i := 0; i < len(intervals); i++ {
 		shit := intervals[i]
+		// 给tmp找到了合适的位置
 		if shit[0] > tmp[1] {
 			res = append(res, tmp)
-			tmp = shit
+			// 后边的直接都放进去，不用再比较了
+			res = append(res, intervals[i:]...)
+			flag = true
+			break
 		} else if shit[1] < tmp[0] {
+			// 还不到tmp的位置
 			res = append(res, shit)
 		} else {
+			// tmp和现在这个有重叠，合并之后需要继续拿着，因为还可能和后边的重叠
 			tmp = mergeTwo(tmp, shit)
 		}
 	}
-	res = append(res, tmp)
+	if !flag {
+		res = append(res, tmp)
+	}
 	return res
 }
 
-//有重叠
+// 有重叠
 func mergeTwo(i1, i2 []int) []int {
 	left := i1[0]
 	if left > i2[0] {
--- LeetCode/Go/p0057_Insert_Interval/code_test.go
+++ LeetCode/Go/p0057_Insert_Interval/code_test.go
@@ -6,6 +6,36 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCode(t *testing.T) {
-	assert.Equal(t, [][]int{{1, 2}, {3, 10}, {12, 16}}, insert([][]int{{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}}, []int{4, 8}))
+func Test_insert(t *testing.T) {
+	type args struct {
+		intervals   [][]int
+		newInterval []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "1",
+			args: args{
+				intervals:   [][]int{{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}},
+				newInterval: []int{4, 8},
+			},
+			want: [][]int{{1, 2}, {3, 10}, {12, 16}},
+		},
+		{
+			name: "2",
+			args: args{
+				intervals:   [][]int{{1, 2}, {6, 7}, {8, 10}, {12, 16}},
+				newInterval: []int{3, 5},
+			},
+			want: [][]int{{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, insert(tt.args.intervals, tt.args.newInterval), "insert(%v, %v)", tt.args.intervals, tt.args.newInterval)
+		})
+	}
 }
